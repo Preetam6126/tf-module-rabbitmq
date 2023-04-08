5e86ce96f1096e54d156e558cb198dba8c9abeec
Preetam <ec2-user@ip-172-31-79-194.ec2.internal>
2023-04-08 05:31:37 +0000

    all is ok

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "aws_spot_instance_request" "rabbitmq" {
   ami           = "data.aws_ami.ami.id"
   instance_type = var.instance_type
   subnet_id     = var.subnet_ids[0]
-  wait_for_fullfillment = true
+  wait_for_fulfillment = true
   
   tags   = merge(
     var.tags,
